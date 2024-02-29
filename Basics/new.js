0043e258e9aa1d7fd56ac44b82e09226848ae81d
Waseeftauqueer <waseeftauqueer.a2022ece@sece.ac.in>
2024-02-29 14:25:04 +0530

    Calculator and Form

--- Basics/new.js
+++ Basics/new.js
@@ -1,4 +1,42 @@
-let a = 1;
-let b=2;
-let c = a+b;
-console.log(c);
+/*let a = 10;
+let b = 20;
+let c = 30;
+if((a>b)&&(a>c))
+{
+    console.log(a);
+}
+else if((b>a)&&(b>c))
+{
+    console.log(b);
+}
+else
+    console.log(c);
+*/
+/*function isInvalid(year, month, day) {
+    return (month === 2 && (day === 30 || day === 31));
+}
+console.log(isInvalid(2024, 2, 29));
+console.log(isInvalid(2022, 2, 31));
+console.log(isInvalid(2022, 3, 31));*/
+
+/*let a =200;
+let h = "hi";
+let he = "hello";
+a%3 ==0 ? consoleh:he;
+if(a%3==0 && a%5==0)
+console.log("Hihello");*/
+
+/*function fun(a)
+{
+    return "Hey"+ " "+a;
+}
+console.log(fun("Velmurugan"));*/
+
+let abc=()=>
+{
+    console.log("faksdjf");
+}
+//console.log(abc());
+
+abc();
+
---
+++ Javascript/clac.js
@@ -0,0 +1,21 @@
+function solve(val)
+{
+    var v = document.getElementById('res');
+    v.value += val;
+}
+function result()
+{
+    var num1 = document.getElementById('res').value;
+    var num2 = eval(num1);
+    document.getElementById('res').value = num2;
+}
+function Clear()
+{
+    var u = document.getElementById('res');
+    u.value = '';
+}
+function back()
+{
+    var e = document.getElementById('res');
+    e.value = e.value.slice(0,-1);
+}

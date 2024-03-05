0cfa2b2da7d9b5f7ec6cb51e1a02a1147e995812
Waseeftauqueer <waseeftauqueer.a2022ece@sece.ac.in>
2024-03-05 22:44:03 +0530

    Portfolio By Waseef Tauqueer

--- Portfolio/portfolio.js
+++ Portfolio/portfolio.js
@@ -17,4 +17,46 @@ document.addEventListener("DOMContentLoaded", function() {
     handleScroll();
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menubro');
+    const navbarLinks = document.querySelectorAll('.navbar a:not(.menubro)');
+    const sections = document.querySelectorAll('.headcontent');
 
+    menuIcon.addEventListener('click', function() {
+        navbarLinks.forEach(link => {
+            link.style.display = link.style.display === 'none' ? 'inline-block' : 'none';
+        });
+
+        sections.forEach(section => {
+            section.style.filter = section.style.filter === 'blur(10px)' ? 'none' : 'blur(10px)';
+        });
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+const cursor = document.querySelectorAll(".cursor");
+const links = document.querySelectorAll(".link, .icons");
+
+window.addEventListener("mousemove", (e) => {
+  
+  let x = e.pageX;
+  let y = e.pageY;
+  
+  cursor.forEach(el => {
+    el.style.left = `${x}px`;
+    el.style.top = `${y}px`;
+    
+    links.forEach(link => {
+      link.addEventListener("mouseenter", () => {
+        el.classList.add("hover");
+      })
+      link.addEventListener("mouseleave", () => {
+        el.classList.remove("hover");
+      })
+    })
+    
+  })
+  
+})
+});
